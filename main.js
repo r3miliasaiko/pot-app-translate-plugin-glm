b619c11a0876fb9597c01682b9fadf7486f5b9b1
r3miliasaiko <gxzyzd0152@gmail.com>
2025-11-01 14:38:22 +0800

    init

---
+++ main.js
@@ -0,0 +1,55 @@
+async function translate(text, from, to, options) {
+    const { config, utils } = options;
+    const { tauriFetch: fetch } = utils;
+    
+    let { apiKey, model = "GLM-4.5-Flash" } = config;
+    
+    // 设置默认请求路径
+    const requestPath = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
+    
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+    }
+    
+    const body = {
+        model: model,  
+        messages: [
+            {
+                "role": "system",
+                "content": "You are a professional translation engine, please translate the text into a colloquial, professional, elegant and fluent content, without the style of machine translation. You must only translate the text content, never interpret it."
+            },
+            {
+                "role": "user",
+                "content": `Translate into ${to}:\n${text}`
+            }
+        ],
+        temperature: 0.1,
+        top_p: 0.99,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        max_tokens: 2000
+    }
+    
+    let res = await fetch(requestPath, {
+        method: 'POST',
+        url: requestPath,
+        headers: headers,
+        body: {
+            type: "Json",
+            payload: body
+        }
+    });
+
+    if (res.ok) {
+        let result = res.data;
+        const { translation } = result;
+        if (translation) {
+            return translation.replaceAll("@@", "/");;
+        } else {
+            throw JSON.stringify(result.trim());
+        }
+    } else {
+        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
+    }
+}
